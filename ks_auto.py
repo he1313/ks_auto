3c01d01b0dbf589d9b21db6963d57ccd99a04b05
he1313 <58430653+he1313@users.noreply.github.com>
2022-06-26 09:00:03 +0800

    Update ks_auto.py

--- ks_auto.py
+++ ks_auto.py
@@ -121,6 +121,7 @@ def post_tw(user,passwd,address='',url='',time=''):#填写体温的总程序
             print('填写失败!')
             send_mail(time+'自动填写体温失败!')#如果不需要发送邮件可以删除此行
     except:
+        print('获取网页信息失败')
         send_mail(time+'自动填写体温失败!')#如果不需要发送邮件可以删除此行
         return "产生异常"
         
