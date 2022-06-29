bff3c88fcce7b994f65a2520bcf00d053fdd939c
he1313 <58430653+he1313@users.noreply.github.com>
2022-06-29 13:42:03 +0800

    Update ks_auto.py

--- ks_auto.py
+++ ks_auto.py
@@ -34,7 +34,7 @@ def send_mail(title='',content=''):
 #***************************************************************
 #发送邮件模块
 header = {
-'Connection': 'keep-alive',
+'Connection': 'close',
 'Host': 'ksxskj.hevttc.edu.cn',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43',
 'Origin': 'https://ksxskj.hevttc.edu.cn',
